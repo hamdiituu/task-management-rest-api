001cc7a552335b7b59f00efde84e2b2330e6549a
Hamdi Tuğ <hamditugmobil@gmail.com>
2021-12-07 00:58:01 +0300

    fix back

--- bin/routes/accountRoute.js
+++ bin/routes/accountRoute.js
@@ -1,7 +1,7 @@
 const route = require('express').Router();
 const accountSchema = require('../schemas/accountSchema');
 const {compareSync, hashSync} = require('bcrypt');
-const {verify} = require('jsonwebtoken');
+const {sign} = require('jsonwebtoken');
 
 const SALT_PREFIX = process.env.SALT_PREFIX || "my_tasks_nodejs";
 const SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
@@ -18,7 +18,7 @@ route.post('/signIn', (req, res) => {
         .then((r) => {
             if (compareSync(`${SALT_PREFIX}${password}`, r.password)) {
                 const payload = {_id: r._id, email: r.email, firstName: r.firstName, lastName: r.lastName};
-                const token = verify(payload, TOKEN_SECRET_KEY, {expiresIn: TOKEN_EXPIRES_IN});
+                const token = sign(payload, TOKEN_SECRET_KEY, {expiresIn: TOKEN_EXPIRES_IN});
                 res.status(200).send({token});
             } else {
                 res.status(401).send('email or password invalid!');
